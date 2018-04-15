afcecb4e7eea3fa425688e3309391fe867672f80
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-15 04:04:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ function getRepositories() {
   req.open("GET", "https://api.github.com/users/octocat/repos");
   req.send()
 }
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList;
+}
 function getCommits(el) {
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
