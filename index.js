07db8ef24fd7d8a5f35671231350409cdb00a15a
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-15 03:56:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function showRepos(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
-document.getElementById("repositories").innerHTML = repoList
   document.getElementById("repositories").innerHTML = repoList
 }
 function getRepositories() {
